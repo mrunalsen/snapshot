f0e76008e7a9bccb38d2efdac855f3dffc4851f2
Mrunal <mrunalpatel2500@gmail.com>
2023-10-09 06:56:03 -0700

    feat: App setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.scss';
+
+function App() {
+
+  return (
+    <>
+      <button className="bg-rose-500 p-5 rounded-lg">hello world</button>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+    },
+  },
+  corePlugins: {
+    container: false
+  },
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '100%',
+          '@screen sm': {
+            maxWidth: '640px',
+          },
+          '@screen md': {
+            maxWidth: '768px',
+          },
+          '@screen lg': {
+            maxWidth: '868px',
+          },
+          '@screen xl': {
+            maxWidth: '868px',
+          },
+        }
+      });
+    }
+  ],
+}
+
